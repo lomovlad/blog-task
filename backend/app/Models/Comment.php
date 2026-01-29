fbad7cd3eae4fa712e967b5a4abb014c9d23e39b
Vladislav <mel@mel.rr>
2026-01-29 20:44:03 +0300

    Добавил модели и миграции

---
+++ backend/app/Models/Comment.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Comment extends Model
+{
+    protected $fillable = [
+        'article_id',
+        'author_name',
+        'content',
+    ];
+
+    public function article(): BelongsTo
+    {
+       return $this->belongsTo(Article::class);
+    }
+}
