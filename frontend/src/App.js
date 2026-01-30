74cd117917043fba24c37d7cfc463ea00048fa51
Vladislav <mel@mel.rr>
2026-01-30 10:39:02 +0300

    Добавлен вывод списка статей на React с использованием API

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import {useEffect, useState} from "react";
+import {getArticles} from "./api/api";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [articles, setArticles] = useState([]);
+
+    useEffect(() => {
+        getArticles().then(data => {
+            setArticles(data);
+        });
+    }, []);
+
+    return (
+        <div>
+            <h1>Статьи</h1>
+
+            {articles.map(article => (
+                <div key={article.id}>
+                    <h2>{article.title}</h2>
+                    <p>{article.content}</p>
+                    <hr/>
+                </div>
+            ))}
+        </div>
+    );
 }
 
 export default App;
