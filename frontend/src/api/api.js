d74ef823e1a3c72520cfee33503180b29ff7bf58
Vladislav <mel@mel.rr>
2026-01-30 10:19:54 +0300

    Добавлен API слой для работы со статьями и комментариями

---
+++ frontend/src/api/api.js
@@ -0,0 +1,21 @@
+const BASE_URL = 'http://localhost:8000/api';
+
+export function getArticles() {
+    return fetch(`${BASE_URL}/articles`)
+        .then(res => res.json());
+}
+
+export function getArticle(id) {
+    return fetch(`${BASE_URL}/articles/${id}`)
+        .then(res => res.json());
+}
+
+export function addComment(articleId, data) {
+    return fetch(`${BASE_URL}/articles/${articleId}/comments`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data)
+    }).then(res => res.json());
+}
